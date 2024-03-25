06535477244abc9c0c66a96d5e1c62e33ced6be0
GuilhermeAlves25 <164787906+GuilhermeAlves25@users.noreply.github.com>
2024-03-25 11:14:21 -0300

    exercicio24.c

--- Lista1/exercicio24.c
+++ Lista1/exercicio24.c
@@ -8,7 +8,7 @@ int main(){
 	printf("Digite a quantia de dinheiro em reais:\n");
 	scanf("%f",&real);
 	
-	printf("Valor digitado em dolares:%.2f\n Valor digitado em marco alemao:%.2f\n valor digitado em libra esterlina:%.2f\n", real*1.80,real*2.00,real*3.57);
+	printf("Valor digitado em dolares:%.2f\n Valor digitado em marco alemao:%.2f\n valor digitado em libra esterlina:%.2f\n", real/1.80,real/2.00,real/3.57);
 	
 	
 	return 0;
