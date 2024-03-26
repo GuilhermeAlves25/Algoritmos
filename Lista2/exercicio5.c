d7079a12be3c26f26c924bc8dc10359797534b57
GuilhermeAlves25 <164787906+GuilhermeAlves25@users.noreply.github.com>
2024-03-26 12:49:13 -0300

    Update exercicio5.c

--- Lista2/exercicio5.c
+++ Lista2/exercicio5.c
@@ -13,20 +13,33 @@ int main(){
 	printf("Escolha um numero de 1 e 4 correspondente a operacao que deseja realizar:");
 	scanf("%d", &escolha);
 	
-	media = (A+B)/2;
-	diferenca = A-B;
-	produto = A*B;
-	divisao = A/B;
 	
+	if (A>B){
+	if (escolha == 1){
+	printf ("Media: %d \n", (A+B)/2);}
+	else if (escolha == 2){
+	printf ("Diferenca: %d \n",A-B);}
+	else if (escolha == 3){
+	printf ("Produto: %d \n", A*B);}
+	else if (escolha == 4 && B != 0){
+	printf ("Divisao: %d \n", A/B);
+	}else {printf("Impossivel dividir por zero!\n");}
+		}
+
+
+	if (A<B){
 	
 	if (escolha == 1){
-	printf ("Media: %d \n", media);}
+	printf ("Media: %d \n", (A+B)/2);}
 	else if (escolha == 2){
-	printf ("Diferenca: %d \n",diferenca);}
+	printf ("Diferenca: %d \n",B-A);}
 	else if (escolha == 3){
-	printf ("Produto: %d \n", produto);}
-	else if (escolha == 4){
-	printf ("Divisao: %d \n", divisao);}
+	printf ("Produto: %d \n", A*B);}
+	else if (escolha == 4 && B != 0){
+	printf ("Divisao: %d \n", A/B);}
+	else {printf("Impossivel dividir por zero!\n");
+	}
+}
 	
 	if (escolha<1 || escolha > 4){
 		printf ("Numero da operacao invalido! *Escolha um numero de 1 e 4.");
